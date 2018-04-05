5d3e426b4de7ef6637e8d4b79d2ad1e547130c85
Warren Klaine <wklaine@mac.com>
2018-04-05 00:00:34 +0000

    init commit

---
+++ hello.rb
@@ -0,0 +1,6 @@
+puts "hello world!" 
+gift = ["puppy", "cat", "treats", "gift card", "letter"]
+puts "All I got for Valentines Day was a #{gift.sample}."
+puts "my new branch really!"
+
+puts "new new"
